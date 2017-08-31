0789575ad2dbec28ab4f0302124156e18ae8f758
ashwini35 <ashwinkumar.gupta@gmail.com>
2017-08-31 15:45:18 +0530

    First time Project setup

---
+++ EnterpriseWebAppDatabase/src/main/java/net/ash/EnterpriseWebAppDatabase/App.java
@@ -0,0 +1,13 @@
+package net.ash.EnterpriseWebAppDatabase;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
